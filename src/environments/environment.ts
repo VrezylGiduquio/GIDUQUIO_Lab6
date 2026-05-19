2195d394cf3dac89322a53be084eb0867fe91426
VrezylGiduquio <vgiduquio@gmail.com>
2026-05-19 21:20:55 +0800

    feat: add live server settings and update backend configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:4000',
-  useFakeBackend: true // set to false to use the real backend api
+  useFakeBackend: false // set to false to use the real backend api
 };
